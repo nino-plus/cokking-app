f0695096b06fb76a1bf08c64e82a7c03eb7c4a26
“kayahara-yuki” <yuanbutaye@icloud.com>
2024-08-28 04:35:24 +0900

    fix(footer): mapで処理するように修正

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,25 @@
 import Link from 'next/link';
 import { Button } from './ui/button';
 
+const navItems = [
+  {
+    label: '食材入力',
+    href: '/ingredients',
+  },
+  {
+    label: 'レシピ結果',
+    href: '/results',
+  },
+  {
+    label: '履歴',
+    href: '/history',
+  },
+  {
+    label: '利用規約',
+    href: '/guide',
+  },
+];
+
 export default function Footer() {
   return (
     <footer className="border-t sticky top-full py-10">
@@ -11,26 +30,13 @@ export default function Footer() {
         <div className="w-full flex md:flex-row flex-col items-center md:justify-between">
           <nav>
             <ul className="flex flex-wrap">
-              <li>
-                <Button variant="ghost" asChild>
-                  <Link href="/ingredients">食材入力</Link>
-                </Button>
-              </li>
-              <li>
-                <Button variant="ghost" asChild>
-                  <Link href="/results">レシピ結果</Link>
-                </Button>
-              </li>
-              <li>
-                <Button variant="ghost" asChild>
-                  <Link href="/history">履歴</Link>
-                </Button>
-              </li>
-              <li>
-                <Button variant="ghost" asChild>
-                  <Link href="/guide">利用規約</Link>
-                </Button>
-              </li>
+              {navItems.map((item) => (
+                <li key={item.label}>
+                  <Button variant="ghost" asChild>
+                    <Link href={item.href}>{item.label}</Link>
+                  </Button>
+                </li>
+              ))}
             </ul>
           </nav>
           <p className="md:mt-0 mt-4">&copy; KAYKOGU</p>
