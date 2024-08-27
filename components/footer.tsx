6b45594fed93edbb0a3fb035f616e1ae534d988f
“kayahara-yuki” <yuanbutaye@icloud.com>
2024-08-27 20:11:57 +0900

    ナビゲーション追加

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,40 @@
 import Link from 'next/link';
-import { ModeToggle } from './mode-toggle';
 import { Button } from './ui/button';
-import { Github, Twitter } from 'lucide-react';
 
 export default function Footer() {
   return (
-    <footer className="border-t sticky top-full">
-      <div className="container h-16 flex items-center justify-between">
-        <Button variant="ghost" asChild>
+    <footer className="border-t sticky top-full py-10">
+      <div className="container flex flex-col items-start">
+        <Button variant="ghost" asChild className="mb-4">
           <Link href="/">LOGO</Link>
         </Button>
-        <p>&copy; KAYKOGU</p>
+        <div className="w-full flex justify-between items-center">
+          <nav>
+            <ul className="flex space-x-2">
+              <li>
+                <Button variant="ghost" asChild>
+                  <Link href="/guide">食材入力</Link>
+                </Button>
+              </li>
+              <li>
+                <Button variant="ghost" asChild>
+                  <Link href="/privacy">レシピ結果</Link>
+                </Button>
+              </li>
+              <li>
+                <Button variant="ghost" asChild>
+                  <Link href="/contact">履歴</Link>
+                </Button>
+              </li>
+              <li>
+                <Button variant="ghost" asChild>
+                  <Link href="/about">利用規約</Link>
+                </Button>
+              </li>
+            </ul>
+          </nav>
+          <p>&copy; KAYKOGU</p>
+        </div>
       </div>
     </footer>
   );
