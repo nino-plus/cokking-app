7dab4d53dfd16097db8316a02909e1b916b0ac16
“kayahara-yuki” <yuanbutaye@icloud.com>
2024-08-27 18:18:03 +0900

    Footerコンポーネントの位置変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,6 @@ export default function RootLayout({
         className={cn(inter.className, 'min-h-dvh')}
         suppressHydrationWarning
       >
-        {' '}
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -31,8 +30,8 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Footer />
         </ThemeProvider>
-        <Footer />
       </body>
     </html>
   );
